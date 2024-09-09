9705bdcdc6c19b6d23df1c161a76e93c3433902e
Said BOUDJELDA <bmscomp@gmail.com>
2024-09-09 21:46:02 +0200

    fix on class AbstractByteWriter

--- src/main/java/net/sf/JRecord/ByteIO/AbstractByteWriter.java
+++ src/main/java/net/sf/JRecord/ByteIO/AbstractByteWriter.java
@@ -2,28 +2,28 @@
 /*  -------------------------------------------------------------------------
  *
  *            Sub-Project: JRecord Common
- *    
- *    Sub-Project purpose: Common Low-Level Code shared between 
+ *
+ *    Sub-Project purpose: Common Low-Level Code shared between
  *                        the JRecord and Record Projects
- *    
+ *
  *                 Author: Bruce Martin
- *    
+ *
  *                License: LGPL 2.1 or latter
- *                
+ *
  *    Copyright (c) 2016, Bruce Martin, All Rights Reserved.
- *   
+ *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
  *    License as published by the Free Software Foundation; either
  *    version 2.1 of the License, or (at your option) any later version.
- *   
+ *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *    GNU Lesser General Public License for more details.
  *
  * ------------------------------------------------------------------------ */
-      
+
 /*
  * @author Bruce Martin
  * Created on 26/08/2005
@@ -40,13 +40,11 @@ import java.nio.file.Paths;
 import net.sf.JRecord.Common.IGetData;
 
 
-
 /**
  * This abstract class is the base class for all <b>Byte~Writer</b>
  * classes
  *
  * @author Bruce Martin
- *
  */
 public abstract class AbstractByteWriter implements IByteRecordWriter {
 
@@ -57,7 +55,6 @@ public abstract class AbstractByteWriter implements IByteRecordWriter {
      * Open file for input
      *
      * @param fileName filename to be opened
-     *
      * @throws IOException any IOerror
      */
     public void open(String fileName) throws IOException {
@@ -69,28 +66,27 @@ public abstract class AbstractByteWriter implements IByteRecordWriter {
      * Open file for input
      *
      * @param outputStream input stream to write
-     *
      * @throws IOException any IOerror
      */
     public abstract void open(OutputStream outputStream)
-    throws IOException;
+            throws IOException;
 
     @Override
     public void write(IGetData lineData) throws IOException {
-    	write(lineData.getData());
+        write(lineData.getData());
     }
 
 
     /* (non-Javadoc)
-	 * @see net.sf.JRecord.ByteIO.IByteRecordWriter#write(byte[])
-	 */
+     * @see net.sf.JRecord.ByteIO.IByteRecordWriter#write(byte[])
+     */
     @Override
-	public abstract void write(byte[] bytes) throws IOException;
+    public abstract void write(byte[] bytes) throws IOException;
 
 
     /* (non-Javadoc)
-	 * @see net.sf.JRecord.ByteIO.IByteRecordWriter#close()
-	 */
+     * @see net.sf.JRecord.ByteIO.IByteRecordWriter#close()
+     */
     @Override
-	public abstract void close() throws IOException;
+    public abstract void close() throws IOException;
 }
