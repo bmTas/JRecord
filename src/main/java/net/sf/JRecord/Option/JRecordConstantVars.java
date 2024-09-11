a0eb101d3b69b6ac2051b0c94396fd7fab31cc7a
Bruce Martin <bm_tas@yahoo.com.au>
2024-09-11 10:33:59 +1000

    Update JRecordConstantVars.java
    Change to use implements for ICopybookDialects, ICobolSplitOptions, IReformatFieldNames

--- src/main/java/net/sf/JRecord/Option/JRecordConstantVars.java
+++ src/main/java/net/sf/JRecord/Option/JRecordConstantVars.java
@@ -36,41 +36,7 @@ import net.sf.JRecord.Numeric.ICopybookDialects;
  * @author Bruce Martin
  *
  */
-public class JRecordConstantVars {
-
-    private JRecordConstantVars() {
-        super();
-    }
-	
-	/* ----------------------------------------------------------------------
-	 * 
-	 * Cobol Dialects:
-	 */
-    public static int FMT_MAINFRAME		  = ICopybookDialects.FMT_MAINFRAME;
-    public static int FMT_FUJITSU		  = ICopybookDialects.FMT_FUJITSU;
-    public static int FMT_BIG_ENDIAN		  = ICopybookDialects.FMT_BIG_ENDIAN;
-    public static int FMT_FS2000			  = ICopybookDialects.FMT_FS2000;
-
-    public static int FMT_FS2000_BE		  = ICopybookDialects.FMT_FS2000_BE;
-    public static int FMT_MAINFRAME_COMMA_DECIMAL = ICopybookDialects.FMT_MAINFRAME_COMMA_DECIMAL;
-    public static int FMT_FUJITSU_COMMA_DECIMAL = ICopybookDialects.FMT_FUJITSU_COMMA_DECIMAL;
-
-    public static int FMT_GNU_COBOL        = ICopybookDialects.FMT_OPEN_COBOL;
-    public static int FMT_GNU_COBOL_MVS    = ICopybookDialects.FMT_GNU_COBOL_MVS;
-    public static int FMT_GNU_COBOL_MF     = ICopybookDialects.FMT_GNU_COBOL_MF;
-
-    public static int FMT_GNU_COBOL_BE     = ICopybookDialects.FMT_GNU_COBOL_BE;
-    public static int FMT_GNU_COBOL_BE_MVS = ICopybookDialects.FMT_OPEN_COBOL_MVS_BE;
-    public static int FMT_GNU_COBOL_BE_MF  = ICopybookDialects.FMT_OC_MICRO_FOCUS_BE;
-    
-	/* -------------------------------------------------------------------------
-	 * 
-	 * Cobol Copybook split options
-	 */
-    public static int SPLIT_NONE              = ICobolSplitOptions.SPLIT_NONE;
-    public static int SPLIT_REDEFINE          = ICobolSplitOptions.SPLIT_REDEFINE;
-    public static int SPLIT_01_LEVEL			 = ICobolSplitOptions.SPLIT_01_LEVEL;
-    public static int SPLIT_HIGHEST_REPEATING = ICobolSplitOptions.SPLIT_HIGHEST_REPEATING;
+public class JRecordConstantVars implements ICopybookDialects, ICobolSplitOptions, IReformatFieldNames {
 
     /* ------------------------------------------------------------------------
      * 
@@ -86,7 +52,7 @@ public class JRecordConstantVars {
     public static int IO_VB_FUJITSU			 = IFileStructureConstants.IO_VB_FUJITSU;
     public static int IO_VB_GNU_COBOL		 = IFileStructureConstants.IO_VB_GNU_COBOL;
     public static int IO_BIN_TEXT			 = IFileStructureConstants.IO_BIN_TEXT;
-    public static int IO_FIXED_LENGTH_CHAR	 = IFileStructureConstants.IO_FIXED_LENGTH_CHAR		        ;
+    public static int IO_FIXED_LENGTH_CHAR	 = IFileStructureConstants.IO_FIXED_LENGTH_CHAR;
     @SuppressWarnings("deprecation")
     public static int IO_VBS					 = IFileStructureConstants.IO_VBS;
 
@@ -97,29 +63,17 @@ public class JRecordConstantVars {
     public static int IO_BIN_CSV				 = IFileStructureConstants.IO_BIN_CSV;
     public static int IO_UNICODE_CSV			 = IFileStructureConstants.IO_UNICODE_CSV;
 
-    public static int IO_CSV_NAME_1ST_LINE	 = IFileStructureConstants.IO_CSV_NAME_1ST_LINE		        ;
+    public static int IO_CSV_NAME_1ST_LINE	 = IFileStructureConstants.IO_CSV_NAME_1ST_LINE;
     public static int IO_BIN_CSV_NAME_1ST_LINE= IFileStructureConstants.IO_BIN_CSV_NAME_1ST_LINE;
-    public static int IO_UNICODE_CSV_NAME_1ST_LINE = IFileStructureConstants.IO_UNICODE_CSV_NAME_1ST_LINE	        ;
+    public static int IO_UNICODE_CSV_NAME_1ST_LINE = IFileStructureConstants.IO_UNICODE_CSV_NAME_1ST_LINE;
 
     public static int IO_NAME_1ST_LINE		 = IFileStructureConstants.IO_NAME_1ST_LINE;
-    public static int IO_BIN_NAME_1ST_LINE	 = IFileStructureConstants.IO_BIN_NAME_1ST_LINE		        ;
+    public static int IO_BIN_NAME_1ST_LINE	 = IFileStructureConstants.IO_BIN_NAME_1ST_LINE;
     public static int IO_UNICODE_NAME_1ST_LINE= IFileStructureConstants.IO_UNICODE_NAME_1ST_LINE;
 
-    public static int IO_STANDARD_UNICODE_TEXT_FIL = IFileStructureConstants.IO_STANDARD_UNICODE_TEXT_FILE        ;
+    public static int IO_STANDARD_UNICODE_TEXT_FIL = IFileStructureConstants.IO_STANDARD_UNICODE_TEXT_FILE;
 
     public static int IO_UNICODE_TEXT	= IFileStructureConstants.IO_UNICODE_TEXT;
     public static int IO_FIXED_LENGTH	= IFileStructureConstants.IO_FIXED_LENGTH;
     public static int IO_TEXT_LINE		= IFileStructureConstants.IO_TEXT_LINE;
-    
-    
-    /* ------------------------------------------------------------------------------
-     * 
-     * Option to reformat Cobol names when converting to other languages
-     * 
-     */
-    public static int RO_LEAVE_ASIS = IReformatFieldNames.RO_LEAVE_ASIS;
-    public static int RO_MINUS_TO_UNDERSCORE = IReformatFieldNames.RO_UNDERSCORE;
-    public static int RO_CAMEL_CASE = IReformatFieldNames.RO_CAMEL_CASE;
-
-
 }
