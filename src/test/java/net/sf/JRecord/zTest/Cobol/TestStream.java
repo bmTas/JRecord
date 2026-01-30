6866de2858bab8bae64a7f960303bdd12ccad147
Said Boudjelda <bmscomp@gmail.com>
2026-01-30 01:48:34 +0100

    fix: Configure Lombok annotation processor for @Slf4j and @Setter sup… (#37)
    * fix: Configure Lombok annotation processor for @Slf4j and @Setter support
    
    The project was failing to compile because the Lombok annotation processor
    was not configured in maven-compiler-plugin. This caused:
    - @Slf4j annotations to not generate the 'log' variable in BasicLine, Line,
      and DbCsvCopybookLoader classes
    - @Setter annotations to not generate setStackSize() method required by
      ISetDropCopybookName interface
    
    This fix adds the annotationProcessorPaths configuration to ensure Lombok
    annotations are properly processed during compilation.
    
    * fix: Replace Stream.toList() with Collectors.toList() for Java 8 compatibility
    
    Stream.toList() was introduced in Java 16, but the project targets Java 8.
    This fixes the CI build failure on JDK 8 by using the Java 8 compatible
    Collectors.toList() method instead.
    
    * adjust formatting
    
    * fix: Add missing test resource MultiRecordTest.cbl for TestContinuousIO01
    
    The test was failing because getClass().getResource('MultiRecordTest.cbl')
    returned null - the resource file existed in the parent directory but not
    in the 'continuous' subdirectory matching the test's package location.

--- src/test/java/net/sf/JRecord/zTest/Cobol/TestStream.java
+++ src/test/java/net/sf/JRecord/zTest/Cobol/TestStream.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,64 +22,59 @@ class TestStream {
 
 	private static final BigDecimal DECIMAL_10 = BigDecimal.valueOf(10);
 	static final String[][] EXPECTED_NEGATIVE_QUANTITIES = {
-			{"69684558", "20", "280", "-1", "-19.00", },
-				{"62684671", "20", "685", "-1", "-69.99",  },
-				{"61664713", "59", "335", "-1", "-17.99",  },	
-	};
-	
-	static final String[][] EXPECTED_PRICE_GREATER_THAN_10 = {
-			{"69684558", "20", "280", "1", "19.00",  },
-			{"62684671", "20", "685", "1", "69.99",  },
-			{"61664713", "59", "335", "1", "17.99",  },
-			{"61684613", "59", "335", "1", "12.99",  },
-			{"69624033", "166", "80", "1", "18.19",  },
-			{"60604100", "166", "80", "1", "13.30",  },
+			{ "69684558", "20", "280", "-1", "-19.00", },
+			{ "62684671", "20", "685", "-1", "-69.99", },
+			{ "61664713", "59", "335", "-1", "-17.99", },
 	};
 
-	
+	static final String[][] EXPECTED_PRICE_GREATER_THAN_10 = {
+			{ "69684558", "20", "280", "1", "19.00", },
+			{ "62684671", "20", "685", "1", "69.99", },
+			{ "61664713", "59", "335", "1", "17.99", },
+			{ "61684613", "59", "335", "1", "12.99", },
+			{ "69624033", "166", "80", "1", "18.19", },
+			{ "60604100", "166", "80", "1", "13.30", },
+	};
+
 	@Test
 	void testFilter1() throws IOException {
 		List<AbstractLine> filteredList = getDTAR020List().filter(
-						line -> line.getFieldValue("DTAR020-QTY-SOLD").asInt() < 0
-					).toList();
-		
+				line -> line.getFieldValue("DTAR020-QTY-SOLD").asInt() < 0).collect(Collectors.toList());
+
 		check(EXPECTED_NEGATIVE_QUANTITIES, filteredList);
-		//printList(filteredList);
+		// printList(filteredList);
 	}
 
-	
 	@Test
 	void testFilter2() throws IOException {
 		List<AbstractLine> filteredList = getDTAR020List().filter(
-						line -> line.getFieldValue("DTAR020-SALE-PRICE").asBigDecimal().compareTo(DECIMAL_10) > 0
-					).toList();
-		
+				line -> line.getFieldValue("DTAR020-SALE-PRICE").asBigDecimal().compareTo(DECIMAL_10) > 0)
+				.collect(Collectors.toList());
+
 		check(EXPECTED_PRICE_GREATER_THAN_10, filteredList);
 		printList(filteredList);
 	}
 
-
 	private Stream<AbstractLine> getDTAR020List() throws IOException, FileNotFoundException {
 		ICobolIOBuilder iob = JRecordInterface1.COBOL.newIOBuilder(
 				JRecordInterface1.COBOL.newCobolCopybookReader()
-					.setCopybookName("DTAR020")
-					.addFreeFormatCobolText(CommonCodeTestCopybooksAndData.DTAR020_COPYBOOK))
+						.setCopybookName("DTAR020")
+						.addFreeFormatCobolText(CommonCodeTestCopybooksAndData.DTAR020_COPYBOOK))
 				.setFont("cp037")
-				.setFileOrganization(IFileStructureConstants.IO_FIXED_LENGTH );
-		
+				.setFileOrganization(IFileStructureConstants.IO_FIXED_LENGTH);
+
 		Stream<AbstractLine> stream = iob.newReader(CommonCodeTestCopybooksAndData.DTAR020_TST1_DATA_FILE)
-										 .stream();
+				.stream();
 		return stream;
 	}
 
-
 	private void check(String[][] expected, List<AbstractLine> filteredList) {
 		assertEquals(expected.length, filteredList.size());
-		
-		for (int i =0; i < expected.length; i++) {
+
+		for (int i = 0; i < expected.length; i++) {
 			AbstractLine l = filteredList.get(i);
 			String[] expectedRow = expected[i];
-			
+
 			assertEquals(expectedRow[0], l.getFieldValue("DTAR020-KEYCODE-NO").asString());
 			assertEquals(expectedRow[1], l.getFieldValue("DTAR020-STORE-NO").asString());
 			assertEquals(expectedRow[2], l.getFieldValue("DTAR020-DEPT-NO").asString());
@@ -86,8 +82,7 @@ class TestStream {
 			assertEquals(expectedRow[4], l.getFieldValue("DTAR020-SALE-PRICE").asString());
 		}
 	}
-	
-	
+
 	private void printList(List<AbstractLine> list) {
 		for (AbstractLine l : list) {
 			System.out.println(""
@@ -97,12 +92,10 @@ class TestStream {
 					+ format(l.getFieldValue("DTAR020-DEPT-NO"))
 					+ format(l.getFieldValue("DTAR020-QTY-SOLD"))
 					+ format(l.getFieldValue("DTAR020-SALE-PRICE"))
-					+ " },"
-					);
-			
+					+ " },");
+
 		}
 	}
-
 
 	String format(IFieldValue v) {
 		return "\"" + v.asString() + "\", ";
